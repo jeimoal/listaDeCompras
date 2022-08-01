ddf15a17e2324e08f91523d9aa7e04e53247b018
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-08-01 12:06:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const ul = document.querySelector('ul');
+let ipt = document.querySelector('#item');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', addItem);
+
+function addItem() {
+	let iptV = ipt.value;
+	ipt.value = "";
+
+	lItem = document.createElement('li');
+	spn = document.createElement('span');
+	btnR = document.createElement('button');
+	btnR.className = "listItem";
+	lItem.appendChild(spn);
+	lItem.appendChild(btnR);
+	spn.textContent = iptV;
+	btnR.textContent = "Remover";
+	ul.appendChild(lItem);
+
+	btnR.addEventListener('click', rmvItem);
+	ipt.focus();
+}
+
+function rmvItem() {
+	let contItem = document.querySelectorAll('.listItem');
+	for (i=0; i < contItem.length; i++){
+		contItem[i].onclick = function() {
+			let btnRmv = this.parentElement;
+			btnRmv.style.display = "none";
+		}
+	}
+}
